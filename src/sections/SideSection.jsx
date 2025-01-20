41b61f2993269c05daaf6824720a7318a3d6e98c
wabjay <omoniyi.success@gmail.com>
2025-01-21 00:59:58 +0100

    Remove component name from the page title

--- src/sections/SideSection.jsx
+++ src/sections/SideSection.jsx
@@ -4,12 +4,23 @@ import Skeleton from "@/components/Skeleton";
 import Link from "next/link";
 import Image from "next/image";
 
-
 const SideSection = ({ page }) => {
-
-
-  console.log(page)
-  // Delete pitch handler
+  const {
+    brandName,
+    brandDescription,
+    componentType,
+    colorPalette,
+    industry,
+    websiteUrl,
+    stacks,
+    type,
+    style,
+    mode,
+    createdAt,
+    font,
+  } = page;
+  const component = componentType[0].replace(" page", "").toLowerCase();
+  const pageName = brandName.includes(component) && brandName.replace(component, "");
 
   // Conditional rendering for page data
   if (!page) return <Skeleton>Loading...</Skeleton>;
@@ -17,79 +28,119 @@ const SideSection = ({ page }) => {
   return (
     <div className="laptop:sticky laptop:top-[66px] py-6 bg-white dark:!bg-black dark:!text-white laptop:h-inherit laptop:mt-[-66px] mb-10 laptop:mb-0 w-full laptop:max-w-[500px] pb-4 px-4 tablet:px-6 laptop:px-10 sidesection__top laptop:pb-[100px] bigScreen:max-w-[700px] bigScreen:pr-[136px]">
       <h1 className="text-24 text-grey-800 dark:!text-white font-semibold mb-2">
-        <Skeleton width={'100%'} height={'20px'}>{page?.brandName}</Skeleton>
+        <Skeleton width={"100%"} height={"20px"}>
+          {pageName}
+        </Skeleton>
       </h1>
       <h2 className="text-[14px] text-grey-600 dark:!text-white mb-6">
-        <Skeleton>{page?.brandDescription}</Skeleton>
+        <Skeleton>{brandDescription}</Skeleton>
       </h2>
 
       <div className="flex flex-col gap-4 p-4 bg-grey-10  dark:!bg-black dark:!text-white border border-grey-50 rounded-xl mb-10">
         <p className="w-full text-grey-600 dark:!text-white grid grid-cols-5 text-14 gap-y-4 font-normal">
           <Skeleton>
-            <span className="col-span-2 text-grey-600 dark:!text-white">Industry</span>
-            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">{page?.industry.join(', ')}</span>
+            <span className="col-span-2 text-grey-600 dark:!text-white">
+              Industry
+            </span>
+            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">
+              {industry.join(", ")}
+            </span>
           </Skeleton>
           <Skeleton>
-            <span className="col-span-2 text-grey-600 dark:!text-white">Component Type</span>
-            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">{page?.componentType.join(', ')}</span>
+            <span className="col-span-2 text-grey-600 dark:!text-white">
+              Component Type
+            </span>
+            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">
+              {componentType.join(", ")}
+            </span>
           </Skeleton>
           <Skeleton>
-            <span className="col-span-2 text-grey-600 dark:!text-white">Stack</span>
-           <span className="col-span-3 text-grey-800 dark:!text-white capitalize"> {page?.stacks.join(', ')}</span>
+            <span className="col-span-2 text-grey-600 dark:!text-white">
+              Stack
+            </span>
+            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">
+              {" "}
+              {stacks.join(", ")}
+            </span>
           </Skeleton>
           <Skeleton>
-            <span className="col-span-2 text-grey-600 dark:!text-white">Type</span>
-            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">{page?.type.join(', ')}</span>
+            <span className="col-span-2 text-grey-600 dark:!text-white">
+              Type
+            </span>
+            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">
+              {type.join(", ")}
+            </span>
           </Skeleton>
           <Skeleton>
-            <span className="col-span-2 text-grey-600 dark:!text-white">Style</span>
-            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">{page?.style.join(', ')}</span>
+            <span className="col-span-2 text-grey-600 dark:!text-white">
+              Style
+            </span>
+            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">
+              {style.join(", ")}
+            </span>
           </Skeleton>
           <Skeleton>
-            <span className="col-span-2 text-grey-600 dark:!text-white">Mode</span>
-            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">{page?.mode}</span>
+            <span className="col-span-2 text-grey-600 dark:!text-white">
+              Mode
+            </span>
+            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">
+              {mode}
+            </span>
           </Skeleton>
           <Skeleton>
-            <span className="col-span-2 text-grey-600 dark:!text-white">Date</span>
-            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">{moment(page?.createdAt).format("LL")}</span>
+            <span className="col-span-2 text-grey-600 dark:!text-white">
+              Date
+            </span>
+            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">
+              {moment(createdAt).format("LL")}
+            </span>
           </Skeleton>
           <Skeleton>
-            <span className="col-span-2 text-grey-600 dark:!text-white">Font</span>
-            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">{page?.font?.join(', ')}</span>
+            <span className="col-span-2 text-grey-600 dark:!text-white">
+              Font
+            </span>
+            <span className="col-span-3 text-grey-800 dark:!text-white capitalize">
+              {font?.join(", ")}
+            </span>
           </Skeleton>
           <Skeleton>
-            <span className="col-span-2 text-grey-600 dark:!text-white">Color Palette</span>
+            <span className="col-span-2 text-grey-600 dark:!text-white">
+              Color Palette
+            </span>
             <span className="col-span-3 ml-6 text-grey-800 dark:!text-white">
-
-  <span className="col-span-3 ml-[-8px] text-grey-800 dark:!text-white">
-  {page?.colorPalette.map((color, index) => (
-    <span
-      key={index}
-      className="relative group ml-[-16px] w-6 h-6 rounded-full inline-block mr-2"
-      style={{ backgroundColor: `#${color}` }}
-    >
-      {/* Tooltip */}
-      <span
-      style={{ backgroundColor: `#${color}` }}
-        className="uppercase absolute left-1/2 transform -translate-x-1/2 -top-8 bg-gray-800 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200"
-      >
-        #{color}
-      </span>
-    </span>
-  ))}
-</span>
-
-</span>          </Skeleton>
+              <span className="col-span-3 ml-[-8px] text-grey-800 dark:!text-white">
+                {colorPalette.map((color, index) => (
+                  <span
+                    key={index}
+                    className="relative group ml-[-16px] w-6 h-6 rounded-full inline-block mr-2"
+                    style={{ backgroundColor: `#${color}` }}
+                  >
+                    {/* Tooltip */}
+                    <span
+                      style={{ backgroundColor: `#${color}` }}
+                      className="uppercase absolute left-1/2 transform -translate-x-1/2 -top-8 bg-gray-800 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200"
+                    >
+                      #{color}
+                    </span>
+                  </span>
+                ))}
+              </span>
+            </span>{" "}
+          </Skeleton>
         </p>
       </div>
-<Link rel="noreferrer" target="_blank" href={page?.websiteUrl} >
-      <div className="px-3 py-2 flex items-center justify-center border-blue-400 border shadow-supportButton bg-blue-200 bg-blueBg hover:bg-blue-500  rounded-lg text-center font-medium  text-white">
-      
-      View Website
-      <Image src="/arrow-button.svg" width={24} height={24} alt="" className="" />
-
-     
-    </div> </Link>
+      <Link rel="noreferrer" target="_blank" href={websiteUrl}>
+        <div className="px-3 py-2 flex items-center justify-center border-blue-400 border shadow-supportButton bg-blue-200 bg-blueBg hover:bg-blue-500  rounded-lg text-center font-medium  text-white">
+          View Website
+          <Image
+            src="/arrow-button.svg"
+            width={24}
+            height={24}
+            alt=""
+            className=""
+          />
+        </div>{" "}
+      </Link>
     </div>
   );
 };

22edd93687db43cf05d763ae9c43cac66d7ba46b
wabjay <omoniyi.success@gmail.com>
2025-02-04 00:21:24 +0100

    Added Posthog

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,19 +1,19 @@
-'use client'
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
-import { ReactNode } from 'react'
+'use client';
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+import { ReactNode } from 'react';
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST as string,
     person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-  })
+  });
 }
 
-interface CSPostHogProviderProp {
-  children: ReactNode
+interface CSPostHogProviderProps {
+  children: ReactNode;
 }
 
-export function CSPostHogProvider({ children }: CSPostHogProviderProp) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+export function CSPostHogProvider({ children }: CSPostHogProviderProps) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
 }
